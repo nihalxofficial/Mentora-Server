3a5b67a5db2223f01890dd6c748101b58a0f869b
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-05-19 00:35:52 +0600

    Atlas connected

---
+++ index.js
@@ -0,0 +1,44 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const dotenv = require("dotenv")
+const cors = require("cors")
+
+dotenv.config()
+const app = express()
+const port = process.env.PORT
+const uri = process.env.MONGO_URI
+
+app.use(cors())
+app.use(express.json())
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("mentora");
+    const courseCollection = db.collection("courses")
+
+    app.get("/courses", async(req, res)=>{
+      const result = await courseCollection.find().toArray();
+      res.send(result);
+    })
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
