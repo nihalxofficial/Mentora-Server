a80494d9db89b426ff73fee86a839f3bc01c3f22
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-05-19 13:32:22 +0600

    JWT token setup and verified for course details page

--- index.js
+++ index.js
@@ -1,51 +1,78 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const express = require('express')
-const dotenv = require("dotenv")
-const cors = require("cors")
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-dotenv.config()
-const app = express()
-const port = process.env.PORT
-const uri = process.env.MONGO_URI
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 
-app.use(cors())
-app.use(express.json())
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+const uri = process.env.MONGO_URI;
+
+// const JWKS = createRemoteJWKSet(new URL(process.env.CLIENT_URL + "/api/auth/jwks"));
+
+app.use(cors());
+app.use(express.json());
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
+const verifyToken = async (req, res, next) => {
+  const { authorization } = req.headers;
+  const token = authorization?.split(" ")[1];
+  if(!token){
+    return res.status(401).json({message: "Unauthorized"})
+  }
+  try {
+    const JWKS = createRemoteJWKSet(new URL(process.env.CLIENT_URL + "/api/auth/jwks"));
+    const { payload } = await jwtVerify(token, JWKS);
+    req.user = payload
+    console.log(req.user);
+    next();
+  } catch (error) {
+    console.error("Token validation failed:", error);
+    return res.status(401).json({message: "Unauthorized"})
+  }
+  
+};
+
 async function run() {
   try {
     await client.connect();
 
     const db = client.db("mentora");
-    const courseCollection = db.collection("courses")
+    const courseCollection = db.collection("courses");
 
-    app.get("/courses", async(req, res)=>{
+    app.get("/courses", async (req, res) => {
       const result = await courseCollection.find().toArray();
       res.send(result);
-    })
-
-    app.get("/courses/:id", async(req, res)=>{
-      const {id} = req.params
-      const result = await courseCollection.findOne({_id: new ObjectId(id)});
+    });
+    app.get("/featured", async (req, res) => {
+      const result = await courseCollection.find().limit(4).toArray();
       res.send(result);
-    })
+    });
 
+    app.get("/courses/:id", verifyToken, async (req, res) => {
+      const { id } = req.params;
+      const result = await courseCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
