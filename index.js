2a96a4ccb9ee6f863bb793b6bf40742674fcf67a
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-05-19 17:28:13 +0600

    Api created for post enrollments and update course data

--- index.js
+++ index.js
@@ -26,20 +26,21 @@ const client = new MongoClient(uri, {
 const verifyToken = async (req, res, next) => {
   const { authorization } = req.headers;
   const token = authorization?.split(" ")[1];
-  if(!token){
-    return res.status(401).json({message: "Unauthorized"})
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
   try {
-    const JWKS = createRemoteJWKSet(new URL(process.env.CLIENT_URL + "/api/auth/jwks"));
+    const JWKS = createRemoteJWKSet(
+      new URL(process.env.CLIENT_URL + "/api/auth/jwks"),
+    );
     const { payload } = await jwtVerify(token, JWKS);
-    req.user = payload
+    req.user = payload;
     console.log(req.user);
     next();
   } catch (error) {
     console.error("Token validation failed:", error);
-    return res.status(401).json({message: "Unauthorized"})
+    return res.status(401).json({ message: "Unauthorized" });
   }
-  
 };
 
 async function run() {
@@ -48,18 +49,21 @@ async function run() {
 
     const db = client.db("mentora");
     const courseCollection = db.collection("courses");
+    const enrollmentCollection = db.collection("enrollments")
 
     app.get("/courses", async (req, res) => {
-      const {search} = req.query
-      console.log(search);
+      const { search } = req.query;
       let result;
-      if(search){
-        result = await courseCollection.find({ title: { $regex: search, $options: "i" } }).toArray();
-      }else{
+      if (search) {
+        result = await courseCollection
+          .find({ title: { $regex: search, $options: "i" } })
+          .toArray();
+      } else {
         result = await courseCollection.find().toArray();
       }
       res.send(result);
     });
+
     app.get("/featured", async (req, res) => {
       const result = await courseCollection.find().limit(4).toArray();
       res.send(result);
@@ -70,6 +74,42 @@ async function run() {
       const result = await courseCollection.findOne({ _id: new ObjectId(id) });
       res.send(result);
     });
+
+    app.get("/enrollments", async(req, res)=> {
+      const result = await enrollmentCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post("/enrollments/:id", verifyToken, async (req, res) => {
+        const { id } = req.params;
+        const enrollmentData = req.body;
+        await courseCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $inc: {enrollCount: 1}, $set: {lastEnrollAt: new Date()}},
+        );
+        const result = await enrollmentCollection.insertOne({...enrollmentData, enrollAt: new Date()})
+        // if (!result) {
+        //   return res.status(404).json({ message: "Data not found" });
+        // }
+        res.send(result);
+      });
+
+
+    // app.patch("/enrollments/:id", async (req, res) => {
+    //     const { id } = req.params;
+    //     console.log(id);
+    //     const enrollmentData = req.body;
+    //     await courseCollection.updateOne(
+    //       { _id: new ObjectId(id) },
+    //       { $inc: {enrollCount: 1}, $set: {lastEnrollAt: new Date()}},
+    //     );
+    //     const result = await enrollmentCollection.insertOne({...enrollmentData, enrollAt: new Date()})
+    //     // if (!result) {
+    //     //   return res.status(404).json({ message: "Data not found" });
+    //     // }
+    //     res.send(result);
+    //   });
+
   } finally {
     // await client.close();
   }
